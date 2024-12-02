8b11555a851253703de00818a906dd595eddd42c
Patrick Rogers <patrick.rogers@library.ca.gov>
2024-12-02 08:17:35 -0800

    Initial Commit

---
+++ src/Advent of Code 2024 Day 01.R
@@ -0,0 +1,13 @@
+### Advent of Code - 2024
+#     Day  1
+#
+#     By Patrick Rogers, California Research Bureau
+#       December 2024
+#
+
+# To Do
+#   1. Item 1
+#   2. Item 2
+
+# Connect to GitHub
+# move to the project directory
